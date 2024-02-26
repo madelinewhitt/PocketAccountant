944a43670a07d9a52e5c819c9f6a2812dd03bb02
Madeline Whitton <madelinewhitton@MacBook-Air-10.local>
2024-02-25 22:51:09 -0500

    Front end dev requirements

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Home() {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                This should say HOME!
+            </h1>
+        </>
+    )
+}
+
+export default Home
