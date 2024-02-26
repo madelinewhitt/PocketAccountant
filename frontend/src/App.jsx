24d14ff7f353e62f5e917cbd356d2d7ac6bf1e96
Madeline Whitton <madelinewhitton@MacBook-Air-10.local>
2024-02-25 23:33:57 -0500

    Added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <Welcome />
+            </>} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
